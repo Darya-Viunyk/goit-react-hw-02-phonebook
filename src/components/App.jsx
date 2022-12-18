5e44dc73b3f89add6a0e811f08e68702b2a459bb
Darya-Viunyk <dar.viunyk@gmail.com>
2022-12-18 22:23:25 +0200

    Added task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { User } from './Contacts/User';
+import { nanoid } from 'nanoid';
+import { Contacts } from './Contacts/Contacts';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+  addUser = data => {
+    const newUser = { ...data, id: nanoid() };
+    this.setState(prevState => ({ users: [...prevState.users, newUser] }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <div>
+          <h3>Name</h3>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </div>
+        <button>Add contact</button>
+        <Contacts />
+        <User />
+      </div>
+    );
+  }
+}
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,26 @@
+import { User } from './User';
+// import PropTypes from 'prop-types';
+
+export const Contacts = () => {
+  return (
+    <did>
+      <h2>Contacts</h2>
+      <ul>
+        {/* {users.map(({ id, name, number }) => {
+        return (
+          <li key={id}>
+            <User name={name} number={number} deleteUser={deleteUser} id={id} />
+          </li>
+        );
+      })} */}
+      </ul>
+    </did>
+  );
+};
+// UsersList.propTypes = {
+//   users: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//     }).isRequired
+//   ).isRequired,
+// };
