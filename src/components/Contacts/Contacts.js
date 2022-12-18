9d8c4b38e9ed103eb388337c6a286c73cc40d2b7
Darya-Viunyk <dar.viunyk@gmail.com>
2022-12-18 22:40:42 +0200

    Added task 2

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,20 +1,20 @@
 import { User } from './User';
 // import PropTypes from 'prop-types';
 
-export const Contacts = () => {
+export const Contacts = ({ users, addUser }) => {
   return (
-    <did>
+    <div>
       <h2>Contacts</h2>
       <ul>
         {/* {users.map(({ id, name, number }) => {
-        return (
-          <li key={id}>
-            <User name={name} number={number} deleteUser={deleteUser} id={id} />
-          </li>
-        );
-      })} */}
+          return (
+            <li key={id}>
+              <User name={name} number={number} addUser={addUser} id={id} />
+            </li>
+          );
+        })} */}
       </ul>
-    </did>
+    </div>
   );
 };
 // UsersList.propTypes = {
--- src/components/Contacts/User.js
+++ src/components/Contacts/User.js
@@ -1,3 +1,20 @@
-export const User = () => {
-  return [];
+export const User = ({ name, number }) => {
+  return (
+    <>
+      <p>
+        name: <span>{name}</span>
+      </p>
+      <p>
+        number: <span>{number}</span>
+      </p>
+      <button
+        type="button"
+        onClick={() => {
+          // addUser(id);
+        }}
+      >
+        Add User
+      </button>
+    </>
+  );
 };
